86a2d4e6e83114f87216eeb20a7066bb04d68f84
mikechambers <mikechambers@gmail.com>
2015-02-13 20:22:08 -0800

    more work

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*jslint vars: true, nomen: true, plusplus: true, continue:true, forin:true */
+/*global $ */
+
+(function () {
+    
+    "use strict";
+    
+    $(document).ready(function () {
+        $(".menu .item").tab();
+    });
+
+}());
