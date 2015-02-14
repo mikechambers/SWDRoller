6308f90f6060b51beb80481919f6debfeb3a71a9
mikechambers <mikechambers@gmail.com>
2015-02-14 00:30:51 -0800

    more dice work

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,15 @@
     
     "use strict";
     
+    var DIE_TYPE = {
+        ABILITY: 0,
+        PROFICIENCY: 1,
+        BOOST: 2,
+        DIFFICULTY: 3,
+        CHALLENGE: 4,
+        SETBACK: 5
+    };
+    
     var RESULT = {
         SUCCESS: "success",
         FAILURE: "failure",
@@ -14,91 +23,82 @@
         DESPAIR: "despair",
         BLANK: "blank"
     };
+    
+    var RESULT_SET = [];
+    RESULT_SET[DIE_TYPE.ABILITY] = [
+        [RESULT.SUCCESS],
+        [RESULT.ADVANTAGE],
+        [RESULT.ADVANTAGE, RESULT.SUCCESS],
+        [RESULT.SUCCESS, RESULT.SUCCESS],
+        [RESULT.ADVANTAGE],
+        [RESULT.SUCCESS],
+        [RESULT.ADVANTAGE, RESULT.ADVANTAGE],
+        [RESULT.BLANK]
+    ];
 
-    var _roll = function () {
-        return this.resultSet[Math.floor(Math.random() * this.resultSet.length)];
-    };
+    RESULT_SET[DIE_TYPE.PROFICIENCY] = [
+        [RESULT.ADVANTAGE, RESULT.ADVANTAGE],
+        [RESULT.ADVANTAGE],
+        [RESULT.ADVANTAGE, RESULT.ADVANTAGE],
+        [RESULT.TRIUMP],
+        [RESULT.SUCCESS],
+        [RESULT.SUCCESS, RESULT.ADVANTAGE],
+        [RESULT.SUCCESS],
+        [RESULT.SUCCESS, RESULT.ADVANTAGE],
+        [RESULT.SUCCESS, RESULT.SUCCESS],
+        [RESULT.SUCCESS, RESULT.ADVANTAGE],
+        [RESULT.SUCCESS, RESULT.SUCCESS],
+        [RESULT.BLANK]
+    ];
+
+
+    RESULT_SET[DIE_TYPE.BOOST] = [
+        [RESULT.ADVANTAGE],
+        [RESULT.SUCCESS, RESULT.ADVANTAGE],
+        [RESULT.ADVANTAGE, RESULT.ADVANTAGE],
+        [RESULT.SUCCESS],
+        [RESULT.BLANK],
+        [RESULT.BLANK]
+    ];
+
+    RESULT_SET[DIE_TYPE.DIFFICULTY] = [
+        [RESULT.THREAT],
+        [RESULT.FAILURE],
+        [RESULT.THREAT, RESULT.FAILURE],
+        [RESULT.THREAT],
+        [RESULT.BLANK],
+        [RESULT.THREAT, RESULT.THREAT],
+        [RESULT.FAILURE, RESULT.FAILURE],
+        [RESULT.THREAT]
+    ];
+
+    RESULT_SET[DIE_TYPE.CHALLENGE] = [
+        [RESULT.THREAT, RESULT.THREAT],
+        [RESULT.THREAT],
+        [RESULT.THREAT, RESULT.THREAT],
+        [RESULT.THREAT],
+        [RESULT.THREAT, RESULT.FAILURE],
+        [RESULT.FAILURE],
+        [RESULT.THREAT, RESULT.FAILURE],
+        [RESULT.FAILURE],
+        [RESULT.FAILURE, RESULT.FAILURE],
+        [RESULT.DESPAIR],
+        [RESULT.FAILURE, RESULT.FAILURE],
+        [RESULT.BLANK]
+    ];
+
+    RESULT_SET[DIE_TYPE.SETBACK] = [
+        [RESULT.FAILURE],
+        [RESULT.FAILURE],
+        [RESULT.THREAT],
+        [RESULT.THREAT],
+        [RESULT.BLANK],
+        [RESULT.BLANK]
+    ];
+
     
-    var DIE = {
-        ABILITY: [
-            [RESULT.SUCCESS],
-            [RESULT.ADVANTAGE],
-            [RESULT.ADVANTAGE, RESULT.SUCCESS],
-            [RESULT.SUCCESS, RESULT.SUCCESS],
-            [RESULT.ADVANTAGE],
-            [RESULT.SUCCESS],
-            [RESULT.ADVANTAGE, RESULT.ADVANTAGE],
-            [RESULT.BLANK]
-        ],
-        
-        PROFICIENCY: [
-            [RESULT.ADVANTAGE, RESULT.ADVANTAGE],
-            [RESULT.ADVANTAGE],
-            [RESULT.ADVANTAGE, RESULT.ADVANTAGE],
-            [RESULT.TRIUMP],
-            [RESULT.SUCCESS],
-            [RESULT.SUCCESS, RESULT.ADVANTAGE],
-            [RESULT.SUCCESS],
-            [RESULT.SUCCESS, RESULT.ADVANTAGE],
-            [RESULT.SUCCESS, RESULT.SUCCESS],
-            [RESULT.SUCCESS, RESULT.ADVANTAGE],
-            [RESULT.SUCCESS, RESULT.SUCCESS],
-            [RESULT.BLANK]
-        ],
-        
-        BOOST: [
-            [RESULT.ADVANTAGE],
-            [RESULT.SUCCESS, RESULT.ADVANTAGE],
-            [RESULT.ADVANTAGE, RESULT.ADVANTAGE],
-            [RESULT.SUCCESS],
-            [RESULT.BLANK],
-            [RESULT.BLANK]
-        ],
-        
-        DIFFICULTY: [
-            [RESULT.THREAT],
-            [RESULT.FAILURE],
-            [RESULT.THREAT, RESULT.FAILURE],
-            [RESULT.THREAT],
-            [RESULT.BLANK],
-            [RESULT.THREAT, RESULT.THREAT],
-            [RESULT.FAILURE, RESULT.FAILURE],
-            [RESULT.THREAT]
-        ],
-        
-        CHALLENGE: [
-            [RESULT.THREAT, RESULT.THREAT],
-            [RESULT.THREAT],
-            [RESULT.THREAT, RESULT.THREAT],
-            [RESULT.THREAT],
-            [RESULT.THREAT, RESULT.FAILURE],
-            [RESULT.FAILURE],
-            [RESULT.THREAT, RESULT.FAILURE],
-            [RESULT.FAILURE],
-            [RESULT.FAILURE, RESULT.FAILURE],
-            [RESULT.DESPAIR],
-            [RESULT.FAILURE, RESULT.FAILURE],
-            [RESULT.BLANK]
-        ],
-        
-        SETBACK: [
-            [RESULT.FAILURE],
-            [RESULT.FAILURE],
-            [RESULT.THREAT],
-            [RESULT.THREAT],
-            [RESULT.BLANK],
-            [RESULT.BLANK]
-        ]
-    };
-    
-    var getDie = function (type) {
-        var out = {
-            name: type,
-            roll: _roll,
-            resultSet: DIE[type]
-        };
-        
-        return out;
+    var _roll = function (resultSet) {
+        return resultSet[Math.floor(Math.random() * resultSet.length)];
     };
     
     var pool = [];
@@ -109,10 +109,10 @@
     
     $(".dice-image").on("click", function (event) {
         
-        var type = event.target.dataset.type;
-        var d = getDie(type.toUpperCase());
-        
-        pool.push(d);
+        var type = parseInt(event.target.dataset.type, 10);
+        pool.push(type);
+        pool.sort();
+        console.log(pool);
     });
     
     $("#roll-button").on("click", function (event) {
@@ -120,8 +120,8 @@
         var out = [];
         var i, k;
         for (i = 0; i < pool.length; i++) {
-            var d = pool[i];
-            var res = d.roll();
+            var resultSet = RESULT_SET[pool[i]];
+            var res = _roll(resultSet);
             
             for (k = 0; k < res.length; k++) {
                 out.push(res[k]);
